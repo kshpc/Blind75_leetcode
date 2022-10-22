34fda36d58b2209ea6c53c80844a357456a2f207
Sri Hari Pada Chandanam Kodi <116411430+kshpc@users.noreply.github.com>
2022-10-22 15:40:06 -0500

    Update 0217-contains-duplicate.cpp

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,7 +5,7 @@ public:
       unordered_set<int> storage; //for storing numnbers from the input array
         for (int i = 0; i < nums.size(); i++) //traversing through given array of nums
         {
-            if (storage.find(nums[i]) != storage.end()) // condition to check if //element is already present
+            if (storage.find(nums[i]) != storage.end()) // condition to check if element is already present
             {
                 return true;
             }
