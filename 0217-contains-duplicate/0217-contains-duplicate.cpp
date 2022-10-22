1796e5f4a4e98c5ad67c9a8a06bcb621b904658b
kshpc <116411430+kshpc@users.noreply.github.com>
2022-10-21 20:02:35 -0500

    Time: 203 ms (44.64%), Space: 51.3 MB (71.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+      unordered_set<int> storage; //for storing numnbers from the input array
+        for (int i = 0; i < nums.size(); i++) //traversing through given array of nums
+        {
+            if (storage.find(nums[i]) != storage.end()) // condition to check if //element is already present
+            {
+                return true;
+            }
+            storage.insert(nums[i]); //insert element if no duplicates are found
+        }
+        
+        return false;
+        
+    }
+};
