a38e323e91b3eae3e1265f8f56fb763bbe43a00f
Sri Hari Pada Chandanam Kodi <116411430+kshpc@users.noreply.github.com>
2022-10-24 11:49:24 -0500

    Time: 50 ms (5.33%), Space: 17.6 MB (6.13%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> mapOfVecIndices; // hash map with key as element of vec & value as vec index
+        for(int i=0; i<numbers.size(); i++)
+        {
+    /*condition to check if difference of target and element in vec is existing in map or not*/
+            if(mapOfVecIndices.find(target-numbers[i])!=mapOfVecIndices.end())
+            {
+                return{mapOfVecIndices[target-numbers[i]]+1, i+1}; //return the indices of elements that sum upto target
+            }
+            mapOfVecIndices.insert(pair<int,int>(numbers[i],i)); //else insert the index into map with element in vec as key
+        } 
+        return {}; //return empty if elements don't sum to target
+    }
+};
