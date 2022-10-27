6cb92bf70ce1d3ddb1660f6ad7eeb7181ae50e5b
Sri Hari Pada Chandanam Kodi <116411430+kshpc@users.noreply.github.com>
2022-10-26 20:17:34 -0500

    Time: 10 ms (96.27%), Space: 10.9 MB (31.91%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> mapOfVecIndices; // hash map with key as element of vec & value as vec index
+        for(int i=0; i<nums.size(); i++)
+        {
+    /*condition to check if difference of target and element in vec is existing in map or not*/
+            if(mapOfVecIndices.find(target-nums[i])!=mapOfVecIndices.end())
+            {
+                return{mapOfVecIndices[target-nums[i]], i}; //return the indices of elements that sum upto target
+            }
+            mapOfVecIndices[nums[i]]=i; //else insert the index into map with element in vec as key
+        } 
+        return {};
+    }
+};
